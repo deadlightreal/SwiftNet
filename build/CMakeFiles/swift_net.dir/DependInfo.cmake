eb9e6f675e4aa3f21f7066ad219675dc241f797f
deadlightreal <richardfabianmain@gmail.com>
2025-03-22 09:24:19 +0100

    it works ig

---
+++ build/CMakeFiles/swift_net.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/admin/Desktop/custom-ethernet-protocol/src/append_to_packet.c" "CMakeFiles/swift_net.dir/append_to_packet.c.o" "gcc" "CMakeFiles/swift_net.dir/append_to_packet.c.o.d"
+  "/Users/admin/Desktop/custom-ethernet-protocol/src/generic_functions.c" "CMakeFiles/swift_net.dir/generic_functions.c.o" "gcc" "CMakeFiles/swift_net.dir/generic_functions.c.o.d"
+  "/Users/admin/Desktop/custom-ethernet-protocol/src/handle_packets.c" "CMakeFiles/swift_net.dir/handle_packets.c.o" "gcc" "CMakeFiles/swift_net.dir/handle_packets.c.o.d"
+  "/Users/admin/Desktop/custom-ethernet-protocol/src/initialize_client_socket.c" "CMakeFiles/swift_net.dir/initialize_client_socket.c.o" "gcc" "CMakeFiles/swift_net.dir/initialize_client_socket.c.o.d"
+  "/Users/admin/Desktop/custom-ethernet-protocol/src/initialize_server_socket.c" "CMakeFiles/swift_net.dir/initialize_server_socket.c.o" "gcc" "CMakeFiles/swift_net.dir/initialize_server_socket.c.o.d"
+  "/Users/admin/Desktop/custom-ethernet-protocol/src/initialize_swift_net.c" "CMakeFiles/swift_net.dir/initialize_swift_net.c.o" "gcc" "CMakeFiles/swift_net.dir/initialize_swift_net.c.o.d"
+  "/Users/admin/Desktop/custom-ethernet-protocol/src/send_packet.c" "CMakeFiles/swift_net.dir/send_packet.c.o" "gcc" "CMakeFiles/swift_net.dir/send_packet.c.o.d"
+  "/Users/admin/Desktop/custom-ethernet-protocol/src/swift_net.c" "CMakeFiles/swift_net.dir/swift_net.c.o" "gcc" "CMakeFiles/swift_net.dir/swift_net.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/swift_net.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libswift_net.a"
+)
